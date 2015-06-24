01874cf11fcf7f4c77fd67366f8434ad8fe92a46
Ivan Krechetov <ikr@ikr.su>
2015-06-24 13:50:49 +0200

    Add the noCssClass assertion

--- tests/massert.js
+++ tests/massert.js
@@ -6,6 +6,11 @@ export default {
         assert(cn.split(' ').indexOf(className) >= 0, `"${className}" not found in "${cn}"`);
     },
 
+    noCssClass(element, className) {
+        const cn = element.className;
+        assert(cn.split(' ').indexOf(className) === -1, `Unexpected "${className}" in "${cn}"`);
+    },
+
     contains(haystack, needle) {
         assert(haystack.indexOf(needle) >= 0, `${needle} not found in ${haystack}`);
     }
