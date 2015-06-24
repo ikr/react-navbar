008771245a6ae9f61be8dc719b8f322a26e60d86
Ivan Krechetov <ikr@ikr.su>
2015-06-24 18:04:00 +0200

    Add the files for the Item implementation

---
+++ tests/Item.spec.js
@@ -0,0 +1,8 @@
+import assert from 'assert';
+import Item from '../src/Item';
+
+describe('Item', () => {
+    it('is a function', () => {
+        assert.strictEqual(typeof Item, 'function');
+    });
+});
