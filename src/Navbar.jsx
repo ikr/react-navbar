5b40828d7651c031281332e37739d6b3bcbd69b3
Ivan Krechetov <ikr@ikr.su>
2015-06-24 13:17:02 +0200

    Add the navbar-header markup

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 
 export default React.createClass({
     render() {
-        return <nav/>;
+        return <nav className="navbar navbar-default">
+            <div className="container-fluid">
+                <div className="navbar-header">
+                    <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#react-navbar-collapse-all" aria-expanded="false">
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                    </button>
+                </div>
+            </div>
+        </nav>;
     }
 });
--- tests/Navbar.spec.js
+++ tests/Navbar.spec.js
@@ -1,6 +1,7 @@
 import assert from 'assert';
 import React from 'react';
 import {addons} from 'react/addons';
+import massert from './massert';
 import Navbar from '../src/Navbar';
 
 const TestUtils = addons.TestUtils;
@@ -19,6 +20,42 @@ describe('Navbar', () => {
 
         it('is a <nav> tag', () => {
             assert.strictEqual(element.tagName, 'NAV');
+        });
+
+        describe('top classes', () => {
+            it('include navbar', () => {
+                massert.cssClass(element, 'navbar');
+            });
+
+            it('include navbar-default', () => {
+                massert.cssClass(element, 'navbar-default');
+            });
+        });
+
+        it('has fluid-container div as an immediate descendant', () => {
+            assert.strictEqual(
+                element.querySelector('div.container-fluid').parentElement,
+                element
+            );
+        });
+
+        it('displays the responsive hamburger header', () => {
+            assert.strictEqual(element.querySelectorAll(
+                '.container-fluid > .navbar-header > button.navbar-toggle > span.icon-bar'
+            ).length, 3);
+        });
+
+        describe('navbar-toggle', () => {
+            let buttonElement;
+
+            beforeEach(() => {
+                buttonElement = element.querySelector('button.navbar-toggle');
+            });
+
+            it('has the #react-navbar-collapse-all as the target', () => {
+                assert.strictEqual(
+                    buttonElement.getAttribute('data-target'), '#react-navbar-collapse-all');
+            });
         });
     });
 });
