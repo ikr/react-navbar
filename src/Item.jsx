9b14ee4ae81e7346bf3227f0429677d4a2093829
Ivan Krechetov <ikr@ikr.su>
2015-06-25 08:36:21 +0200

    Finalize the Item

--- src/Item.jsx
+++ src/Item.jsx
@@ -1,1 +1,19 @@
-export default () => {};
+import React from 'react';
+import {IntlMixin} from 'react-intl';
+
+export default React.createClass({
+    propTypes: {
+        title: React.PropTypes.string.isRequired,
+        href: React.PropTypes.string.isRequired
+    },
+
+    mixins: [IntlMixin],
+
+    render() {
+        return <li>
+            <a href={this.props.href}>
+                {this.getIntlMessage(this.props.title)}
+            </a>
+        </li>;
+    }
+});
--- tests/Item.spec.js
+++ tests/Item.spec.js
@@ -1,8 +1,59 @@
 import assert from 'assert';
+import React from 'react';
+import {addons} from 'react/addons';
 import Item from '../src/Item';
+
+const TestUtils = addons.TestUtils;
 
 describe('Item', () => {
     it('is a function', () => {
         assert.strictEqual(typeof Item, 'function');
+    });
+
+    it('declares the title required string property', () => {
+        assert.strictEqual(Item.propTypes.title, React.PropTypes.string.isRequired);
+    });
+
+    it('declares the href required string property', () => {
+        assert.strictEqual(Item.propTypes.href, React.PropTypes.string.isRequired);
+    });
+
+    describe('HTML', () => {
+        let element;
+
+        beforeEach(() => {
+            element = TestUtils.renderIntoDocument(React.createElement(Item, {
+                title: 'menu.nunu',
+                href: '#/actions/nunu',
+
+                messages: {
+                    menu: {nunu: 'Nü-nü'}
+                }
+            })).getDOMNode();
+        });
+
+        it('is an <li>', () => {
+            assert.strictEqual(element.tagName, 'LI');
+        });
+
+        describe('nested anchor', () => {
+            let aElement;
+
+            beforeEach(() => {
+                aElement = element.querySelector('a');
+            });
+
+            it('is an immediate descendant', () => {
+                assert.strictEqual(aElement.parentElement, element);
+            });
+
+            it('has the href attribute set to the href prop value', () => {
+                assert.strictEqual(aElement.getAttribute('href'), '#/actions/nunu');
+            });
+
+            it('nests the localized title prop as the inner text', () => {
+                assert.strictEqual(aElement.textContent, 'Nü-nü');
+            });
+        });
     });
 });
