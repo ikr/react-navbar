600bba5e5c537b65264f6b840e98e6f9814283a9
Ivan Krechetov <ikr@ikr.su>
2015-06-25 09:51:39 +0200

    Implement the Dropdown

--- src/Dropdown.jsx
+++ src/Dropdown.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import {IntlMixin} from 'react-intl';
+import Item from './Item';
 
 export default React.createClass({
     propTypes: {
@@ -6,11 +8,23 @@ export default React.createClass({
         items: React.PropTypes.array.isRequired
     },
 
+    mixins: [IntlMixin],
+
     render() {
+        const items = this.props.items.map((item, index) => {
+            const ref = `i${index}`;
+            return <Item {...item} ref={ref} key={ref}/>;
+        });
+
         return <li className="dropdown">
             <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                {this.getIntlMessage(this.props.title)}
                 <span className="caret"></span>
             </a>
+
+            <ul className="dropdown-menu">
+                {items}
+            </ul>
         </li>;
     }
 });
--- tests/Dropdown.spec.js
+++ tests/Dropdown.spec.js
@@ -2,6 +2,7 @@ import assert from 'assert';
 import React from 'react';
 import {addons} from 'react/addons';
 import Dropdown from '../src/Dropdown';
+import Item from '../src/Item';
 
 const TestUtils = addons.TestUtils;
 
@@ -24,12 +25,21 @@ describe('Dropdown', () => {
 
         beforeEach(() => {
             component = TestUtils.renderIntoDocument(React.createElement(Dropdown, {
-                title: 'leMenu.root.dd1',
+                title: 'leMenu.root.dd1Title',
 
                 items: [
                     {title: 'leMenu.root.dd1.i1', href: '/action-1'},
                     {title: 'leMenu.root.dd1.i2', href: '/action-2'}
-                ]
+                ],
+
+                messages: {
+                    leMenu: {
+                        root: {
+                            dd1Title: 'Drop-down-1',
+                            dd1: {i1: 'Item-1', i2: 'Item-2'}
+                        }
+                    }
+                }
             }));
 
             element = component.getDOMNode();
@@ -54,8 +64,39 @@ describe('Dropdown', () => {
                 assert.strictEqual(aElement.getAttribute('href'), '#');
             });
 
+            it('nests the title translation', () => {
+                assert.strictEqual(aElement.textContent, 'Drop-down-1');
+            });
+
             it('nests the caret span', () => {
                 assert(aElement.querySelector('span.caret'));
+            });
+        });
+
+        describe('items container', () => {
+            let ulElement;
+
+            beforeEach(() => {
+                ulElement = element.querySelector('.dropdown-menu');
+            });
+
+            it('is a <ul>', () => {
+                assert.strictEqual(ulElement.tagName, 'UL');
+            });
+
+            it('renders a li for each item', () => {
+                assert.strictEqual(ulElement.querySelectorAll('li').length, 2);
+            });
+
+            it('assigns a ref to the last item', () => {
+                assert.strictEqual(
+                    ulElement.querySelectorAll('li')[1].getAttribute('data-reactid'),
+                    component.refs.i1.getDOMNode().getAttribute('data-reactid')
+                );
+            });
+
+            it('nests an Item instance as the first item', () => {
+                assert(TestUtils.isCompositeComponentWithType(component.refs.i0, Item));
             });
         });
     });
