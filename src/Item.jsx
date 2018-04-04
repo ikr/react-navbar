c2086229cdefb126252c4d02bb6304d1349874d3
vartemiev <vartemiev@xiag.ch>
2018-04-04 16:59:26 +0700

    Added `target` attribute to navbar item. Updated test

--- src/Item.jsx
+++ src/Item.jsx
@@ -4,14 +4,19 @@ import {IntlMixin} from 'react-intl';
 export default React.createClass({
     propTypes: {
         title: React.PropTypes.string.isRequired,
-        href: React.PropTypes.string.isRequired
+        href: React.PropTypes.string.isRequired,
+        target: React.PropTypes.string
+    },
+
+    defaultProps: {
+        target: '_self'
     },
 
     mixins: [IntlMixin],
 
     render() {
         return <li>
-            <a href={this.props.href}>
+            <a href={this.props.href} target={this.props.target}>
                 {this.getIntlMessage(this.props.title)}
             </a>
         </li>;
