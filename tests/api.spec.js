e2e3e93ad6103a884b9c118b3965351096da59fb
Ivan Krechetov <ikr@ikr.su>
2018-07-24 15:10:14 +0200

    Replace the obsolete implementation

--- tests/api.spec.js
+++
@@ -1,9 +0,0 @@
-import assert from 'assert'
-import api from '../src/index'
-import Navbar from '../src/Navbar'
-
-describe('Public API', () => {
-    it('is basically the Navbar class', () => {
-        assert.strictEqual(api, Navbar)
-    })
-})
--- tests/helpers/enzyme.js
+++ tests/helpers/enzyme.js
@@ -1,8 +1,10 @@
-import {JSDOM} from 'jsdom'
-import 'raf/polyfill'
+const {JSDOM} = require('jsdom')
+const {configure} = require('enzyme')
+const Adapter = require('enzyme-adapter-react-16')
 
 const jsdom = new JSDOM('<html><body><div id="main"></div></body></html>')
-
 global.window = jsdom.window
 global.document = jsdom.window.document
 global.navigator = jsdom.window.navigator
+
+configure({adapter: new Adapter()})
