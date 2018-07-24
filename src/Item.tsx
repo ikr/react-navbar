fb5d65dd2b5df673c466274ee2b3e864639bd82a
Ivan Krechetov <ikr@ikr.su>
2018-07-24 07:06:33 +0200

    Add the TS implementation of Item

---
+++ src/Item.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import { FormattedMessage } from 'react-intl'
+
+export default function Item(props: Props) {
+    const { href, target, title } = props
+
+    return (
+        <li>
+            <a {...{ href, target }}>
+                <FormattedMessage id={title} />
+            </a>
+        </li>
+    )
+}
+
+export interface Props {
+    href: string,
+    target?: string,
+    title: string
+}
---
+++ tests/Item.spec.tsx
@@ -0,0 +1,49 @@
+import * as assert from 'assert'
+import * as React from 'react'
+import { IntlProvider } from 'react-intl'
+import { mount } from 'enzyme'
+import Item, { Props } from '../src/Item'
+
+function messages(): { [id: string]: string } {
+    return { 'menu.nunu': 'Nü-nü' }
+}
+
+function props(): Props {
+    return {
+        title: 'menu.nunu',
+        href: '#/actions/nunu',
+        target: '_target'
+    }
+}
+
+describe('Item', () => {
+    const wrapper = mount(
+        <IntlProvider locale='ru' messages={messages()}>
+            <Item {...props()} />
+        </IntlProvider>
+    ).find('Item')
+
+    it('is an LI on top level', () => {
+        assert.strictEqual(wrapper.childAt(0).type(), 'li')
+    })
+
+    describe('nested anchor', () => {
+        const aWrapper = wrapper.find('li > a')
+
+        it('is present', () => {
+            assert(aWrapper.exists())
+        })
+
+        it('has the href prop value assigned', () => {
+            assert.strictEqual(aWrapper.prop('href'), '#/actions/nunu')
+        })
+
+        it('has the target prop value assigned', () => {
+            assert.strictEqual(aWrapper.prop('target'), '_target')
+        })
+
+        it('nests the localized title prop as the inner text', () => {
+            assert.strictEqual(aWrapper.text(), 'Nü-nü')
+        })
+    })
+})
