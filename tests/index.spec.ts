c28dbd00b3f56dc670d66736d106c87c0ddea9ed
Ivan Krechetov <ikr@ikr.su>
2018-07-24 15:08:40 +0200

    Export the npm module public API

---
+++ tests/index.spec.ts
@@ -0,0 +1,9 @@
+import * as assert from 'assert'
+import api from '../src/index'
+import Navbar from '../src/Navbar'
+
+describe('Public API', () => {
+    it('is basically the Navbar class', () => {
+        assert.strictEqual(api, Navbar)
+    })
+})
